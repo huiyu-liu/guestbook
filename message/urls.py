aedfb7b45c0ed26ce1fb55b778e9d1be2c930149
Huiyu Liu <11235029@dcsh.tp.edu.tw>
2024-07-04 16:11:57 +0800

    查看留言，新增留言，登入

--- message/urls.py
+++ message/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     # message/   => 留言列表
     path('', views.MessageList.as_view(), name='msg_list'),
+    # message/1/, message/2/, ...
+    path('<int:pk>/', views.MessageRead.as_view(), name='msg_read'),
+    # message/create/
+    path('create/', views.MessageNew.as_view(), name='msg_create'),
+
 ]
--- message/views.py
+++ message/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Message
 
 # Create your views here.
 class MessageList(ListView):
     model = Message
 
-    # 應用程式/資料模型_list.html
-    # message/message_list.html
+    # 頁面範本檔案名稱: 應用程式/資料模型_list.html => message/message_list.html
+    # 頁面範本變數名稱: 資料模型_list => message_list
 
-    # 資料模型_list
+class MessageRead(DetailView):
+    model = Message
+
+    # 頁面範本檔案名稱: 應用程式/資料模型_detail.html => message/message_detail.html
+    # 頁面範本變數名稱: 資料模型 => message
+
+class MessageNew(CreateView):
+    model = Message
+    fields = ['user', 'receipt', 'subject', 'content']
+    success_url = '/message/'
+    # fields = '__all__'
+
+    # 頁面範本檔案名稱: 應用程式/資料模型_form.html => message/message_form.html
+    # 頁面範本變數名稱: form
