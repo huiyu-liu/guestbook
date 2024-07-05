ab729ddb1b450aee99f73b49adc0fad143ac87b2
Huiyu Liu <11235029@dcsh.tp.edu.tw>
2024-07-05 10:42:45 +0800

    登出、刪除留言

--- message/urls.py
+++ message/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk>/', views.MessageRead.as_view(), name='msg_read'),
     # message/create/
     path('create/', views.MessageNew.as_view(), name='msg_create'),
-
+    # message/1/delete/, message/2/delete, ...
+    path('<int:pk>/delete/', views.MessageDelete.as_view(), name='msg_delete'),
 ]
--- message/views.py
+++ message/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
-from django.urls import reverse_lazy
 from .models import Message
+from django.urls import reverse_lazy
 
 # Create your views here.
 class MessageList(ListView):
     model = Message
+    ordering = ['user']
 
     # 頁面範本檔案名稱: 應用程式/資料模型_list.html => message/message_list.html
     # 頁面範本變數名稱: 資料模型_list => message_list
@@ -19,8 +20,15 @@ class MessageRead(DetailView):
 class MessageNew(CreateView):
     model = Message
     fields = ['user', 'receipt', 'subject', 'content']
-    success_url = '/message/'
+    success_url = reverse_lazy('msg_list')
     # fields = '__all__'
 
     # 頁面範本檔案名稱: 應用程式/資料模型_form.html => message/message_form.html
     # 頁面範本變數名稱: form
+
+class MessageDelete(DeleteView):
+    model = Message
+    success_url = reverse_lazy('msg_list')
+
+    # 頁面範本檔案名稱: 應用程式/資料模型_confirm_delete.html => message/message_confirm_delete.html
+    # 頁面範本變數名稱: form, object, 資料模型(小寫)
